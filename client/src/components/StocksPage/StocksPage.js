e814f297adc38c63f9295d3510d88da58c3bee67
S Krishna Kishore <krishnakishore.s219@gmail.com>
2021-05-24 06:33:28 +0530

    added stocks page and removed everything related to auth

---
+++ client/src/components/StocksPage/StocksPage.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import ListStock from "../ListStock/ListStock";
+import StockInfo from "../StockInfo/StockInfo";
+const StocksPage = () => {
+  let [tarStock, setTarStock] = useState("");
+  const targetStock = (tar) => {
+    setTarStock(tar);
+  };
+  if (tarStock.trim().length != 0) {
+    return <StockInfo targStock={tarStock} />;
+  }
+  return (
+    <div>
+      <SearchBar targetted={targetStock} />
+      <ListStock />
+    </div>
+  );
+};
+
+export default StocksPage;
